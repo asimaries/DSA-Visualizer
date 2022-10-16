d7187faa0edb7549c1251612e08fec6c4728c282
asimaries <logxtech59@gmail.com>
2022-10-16 19:03:50 +0530

    animations added

--- scriptqueue.js
+++ scriptqueue.js
@@ -1,68 +1,79 @@
-const ul = document.getElementById("canvas");
-const fele = document.getElementById("frontval");
-const bele = document.getElementById("backval");
-const inp = document.getElementsByTagName("input");
-let element = `<div class="arrow"></div>
-                <div class="ele"></div>`;
-function setFront() {
-	ul.firstElementChild.firstElementChild.hidden = true;
-	fele.innerText = ul.firstElementChild.innerText;
-}
-function setBack() {
-	bele.innerText = ul.lastElementChild.innerText;
-}
-function setSIZE() {
-	const siz = document.getElementById("size");
-	siz.textContent = size;
-}
-let size = 3, usersize = 15;
-function userSETsize() {
-	usersize = Number(inp[1].value);
-}
-function push() {
-	if (size === usersize) {
-		document
-			.getElementById("error")
-			.appendChild(document.createTextNode("QUEUE Overflow!"));
-		return;
-	}
-	document.getElementById("error").textContent = "";
-	const val = inp[0].value;
-	inp[0].value = "";
-	console.log(val);
-	const li = document.createElement("li");
-	li.innerHTML = element;
-	li.lastElementChild.textContent = val;
-	ul.appendChild(li);
-	setBack();
-	setFront();
-	size += 1;
-	setSIZE();
-}
-function pop() {
-	if (size === 0) {
-		document
-			.getElementById("error")
-			.appendChild(document.createTextNode("QUEUE is alreay EMPTY!"));
-	}
-	ul.removeChild(ul.firstElementChild);
-	size -= 1;
-	if (size === 0) {
-		fele.innerText = "null";
-		bele.innerText = "null";
-	} else {
-		setFront();
-		setBack();
-	}
-	setSIZE();
-}
-inp[0].addEventListener("keypress", (event) => {
-	if (event.keyCode === 13) {
-		push();
-	}
-});
-inp[1].addEventListener("keypress", (event) => {
-	if (event.keyCode === 13) {
-		userSETsize();
-	}
-});
+const ul = document.getElementsByClassName("canvas")[0];
+			const fele = document.getElementById("frontval");
+			const bele = document.getElementById("backval");
+			const inp = document.getElementsByTagName("input");
+			let size = 3,usersize = 10;
+			let element = `<span class="arrow"></span>
+			<span class="ele"></span>`;
+			inp[1].value = usersize;
+			function setFront() {
+				ul.firstElementChild.firstElementChild.hidden = true;
+				fele.innerText = ul.firstElementChild.innerText;
+			}
+			function setBack() {
+				bele.innerText = ul.lastElementChild.innerText;
+			}
+			function setSIZE() {
+				const siz = document.getElementById("size");
+				siz.textContent = size;
+			}
+			function userSETsize() {
+				usersize = Number(inp[1].value);
+			}
+			function push() {
+				if (size === usersize) {
+					document
+						.getElementById("error")
+						.appendChild(
+							document.createTextNode("QUEUE Overflow!"),
+						);
+					return;
+				}
+				document.getElementById("error").textContent = "";
+				const val = inp[0].value;
+				inp[0].value = "";
+				console.log(val);
+				const li = document.createElement("li");
+				li.innerHTML = element;
+				li.lastElementChild.textContent = val;
+				ul.appendChild(li);
+				setTimeout(() => {
+					li.classList.add("show");
+				}, 100);
+				setBack();
+				setFront();
+				size += 1;
+				setSIZE();
+			}
+			function pop() {
+				if (size === 0) {
+					document.getElementById("error").appendChild(
+							document.createTextNode("QUEUE is alreay EMPTY!"),
+						);
+				return;
+				}
+				document.getElementById("error").textContent = "";
+				ul.firstElementChild.classList.remove("show");
+				setTimeout(() => {
+					ul.firstElementChild.remove();
+					size -= 1;
+					if (size === 0) {
+						fele.innerText = "null";
+						bele.innerText = "null";
+					} else {
+						setFront();
+						setBack();
+					}
+					setSIZE();
+				}, 400);
+			}
+			inp[0].addEventListener("keypress", (event) => {
+				if (event.keyCode === 13) {
+					push();
+				}
+			});
+			inp[1].addEventListener("keypress", (event) => {
+				if (event.keyCode === 13) {
+					userSETsize();
+				}
+			});
--- scriptstack.js
+++ scriptstack.js
@@ -1,45 +1,58 @@
 let size = 3;
-const inp = document.getElementsByTagName("input")[0];
+const inp = document.getElementsByTagName("input");
 const ul = document.getElementById("canvas").firstElementChild;
 const siz = document.getElementById("size");
+
 function setSIZE() {
-    siz.textContent = size;
+	siz.textContent = size;
 }
 function setTop() {
-    if (size === 0) document.getElementById("topval").innerText = "NULL";
-    else
-        document.getElementById("topval").innerText =
-            document.getElementById(
-                "canvas"
-            ).firstElementChild.firstElementChild.innerText;
+	if (size === 0) document.getElementById("topval").innerText = "NULL";
+	else
+		document.getElementById("topval").innerText =
+			document.getElementById(
+				"canvas",
+			).firstElementChild.firstElementChild.innerText;
 }
 function pop() {
-    if (size === 0) {
-        document
-            .getElementById("error")
-            .appendChild(document.createTextNode("STACK underflow!"));
-        return;
-    }
-    size -= 1;
-    ul.removeChild(ul.firstElementChild);
-    setSIZE();
-    setTop();
+	if (size === 0) {
+		document
+			.getElementById("error")
+			.appendChild(document.createTextNode("STACK underflow!"));
+		return;
+	}
+	document.getElementById("error").textContent = "";
+	size -= 1;
+	ul.firstElementChild.classList.remove("show");
+	setTimeout(() => {
+		ul.removeChild(ul.firstElementChild);
+	}, 400);
+	setSIZE();
+	setTop();
 }
 function push() {
-    if (size === 13) {
-        document
-            .getElementById("error")
-            .appendChild(document.createTextNode("STACK Overflow!"));
-        return;
-    }
-    document.getElementById("error").textContent = "";
-    const val = inp.value;
-    inp.value = "";
-    console.log(val);
-    const li = document.createElement("li");
-    li.textContent = val;
-    ul.prepend(li);
-    size += 1;
-    setSIZE();
-    setTop();
+	if (size === 10) {
+		document
+			.getElementById("error")
+			.appendChild(document.createTextNode("STACK Overflow!"));
+		return;
+	}
+	document.getElementById("error").textContent = "";
+	const val = inp[0].value;
+	inp[0].value = "";
+	console.log(val);
+	const li = document.createElement("li");
+	li.textContent = val;
+	ul.prepend(li);
+	setTimeout(() => {
+		li.classList.add("show");
+	}, 100);
+	size += 1;
+	setSIZE();
+	setTop();
 }
+inp[0].addEventListener("keypress", (event) => {
+	if (event.keyCode === 13) {
+		push();
+	}
+});
